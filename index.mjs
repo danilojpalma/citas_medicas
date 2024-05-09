c9198722f79f3182ccd5f01c4f68663c2a598ec4
Danilo J. Palma <dan.palmav@duocuc.cl>
2024-05-09 00:21:01 -0400

    improve logic, divide array

--- index.mjs
+++ index.mjs
@@ -8,43 +8,64 @@ import _ from 'lodash';
 import chalk from 'chalk';
 
 app.use(express.json());
-const timestamp = moment().format('MMMM Do YYYY, h:mm:ss a');
 
 let usuarios = [];
 
 async function registrarUsuario() {
-    for (let i = 0; i < 12; i++) {
     try {
+        // Solicita 12 resultados de la API
         const response = await axios.get('https://randomuser.me/api/?results=12');
-        const usuario = response.data.results[i];
-        const id = uuidv4().slice(0, 6);
-        usuarios.push({...usuario, id, timestamp });
-        console.log(chalk.bgWhite.blue(`#${i + 1}. Nombre: ${usuario.name.first} - Apellido: ${usuario.name.last} - ID: ${id} - timestamp: ${timestamp}`));
+        const usuariosData = response.data.results;
+
+        // Registra los usuarios en el array de usuarios
+        usuariosData.forEach((usuario) => {
+            const id = uuidv4().slice(0, 6);
+            const timestamp = moment().format('MMMM Do YYYY, h:mm:ss a');
+            usuarios.push({first: usuario.name.first, last: usuario.name.last, gender: usuario.gender, id, timestamp});
+        });
+
+        // Filtra los usuarios por género después de registrar todos los usuarios
+        const usuariosFemeninos = _.filter(usuarios, usuario => usuario.gender === 'female');
+        const usuariosMasculinos = _.filter(usuarios, usuario => usuario.gender === 'male');
+
+        console.log(chalk.bgWhite.blue('Pacientes Registrados:'));
+        // Imprime los usuarios femeninos
+        console.log(chalk.bgWhite.blue('Mujeres:'));
+        let contadorMujeres = 1;
+        usuariosFemeninos.forEach(usuario => {
+            console.log(chalk.bgWhite.blue(`${contadorMujeres++}. Nombre: ${usuario.first} - Apellido: ${usuario.last} - ID: ${usuario.id} - timestamp: ${usuario.timestamp}`));
+        });
+
+        // Imprime los usuarios masculinos
+        console.log(chalk.bgWhite.blue('Hombres:'));
+        let contadorHombres = 1;
+        usuariosMasculinos.forEach(usuario => {
+            console.log(chalk.bgWhite.blue(`${contadorHombres++}. Nombre: ${usuario.first} - Apellido: ${usuario.last} - ID: ${usuario.id} - timestamp: ${usuario.timestamp}`));
+        });
+
     } catch (error) {
         console.error('Error al registrar usuario:', error);
     }
 }
-}
-
-
 
 registrarUsuario();
 
 app.get('/usuarios', (req, res) => {
 
-     const usuariosPorGenero = _.groupBy(usuarios, 'gender');
+     const usuariosFemeninos = _.filter(usuarios, usuario => usuario.gender === 'female');
+     const usuariosMasculinos = _.filter(usuarios, usuario => usuario.gender === 'male');
 
-     let respuestaHTML = '<h1>Usuarios Registrados</h1>';
+     let respuestaHTML = '<h1>Pacientes Registrados</h1>';
      respuestaHTML += '<h2>Mujeres</h2>';
      let contadorMujeres = 1;
-     usuariosPorGenero['female'].forEach(usuario => {
-         respuestaHTML += `<p>${contadorMujeres++}. Nombre: ${usuario.name.first} - Apellido: ${usuario.name.last} - ID: ${usuario.id} - timestamp: ${timestamp}</p>`;
+     usuariosFemeninos.forEach(usuario => {
+         respuestaHTML += `<p>${contadorMujeres++}. Nombre: ${usuario.first} - Apellido: ${usuario.last} - ID: ${usuario.id} - timestamp: ${usuario.timestamp}</p>`;
      });
  
      respuestaHTML += '<h2>Hombres</h2>';
      let contadorHombres = 1;
-     usuariosPorGenero['male'].forEach(usuario => {
-         respuestaHTML += `<p>${contadorHombres++}. Nombre: ${usuario.name.first} - Apellido: ${usuario.name.last} - ID: ${usuario.id} - timestamp: ${timestamp}</p>`;
+     usuariosMasculinos.forEach(usuario => {
+         respuestaHTML += `<p>${contadorHombres++}. Nombre: ${usuario.first} - Apellido: ${usuario.last} - ID: ${usuario.id} - timestamp: ${usuario.timestamp}</p>`;
      });
 
      
